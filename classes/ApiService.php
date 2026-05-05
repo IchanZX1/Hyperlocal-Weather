5303527571f5ccf3945db647f08a2db46720af69
Alif Taran Ihsan <aliftaranihsan@gmail.com>
2026-05-05 22:19:14 +0700

    attempt to bypass cloudflare bot detection

--- classes/ApiService.php
+++ classes/ApiService.php
@@ -37,9 +37,19 @@ class ApiService
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            "Accept: application/json, text/plain, */*",
+            "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Cache-Control: no-cache",
+            "Connection: keep-alive",
+            "Sec-Ch-Ua: \"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+            "Sec-Ch-Ua-Mobile: ?0",
+            "Sec-Ch-Ua-Platform: \"Windows\"",
+            "Referer: https://api-faa.my.id/"
+        ]);
 
         $response = curl_exec($ch);
 
